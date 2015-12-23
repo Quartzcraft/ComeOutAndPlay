75347c5fb10b8779c231c9bdbdb385f62699e677
ZeWildGuy <njb3112@iCloud.com>
2015-12-23 20:31:23 +1000

    Created very basic and empty main class

---
+++ src/uk/co/quartzcraft/coap/COAP.java
@@ -0,0 +1,16 @@
+package uk.co.quartzcraft.coap;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class COAP extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+}
