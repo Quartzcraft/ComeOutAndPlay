30a87b9f1a73c6399dc8eaaf019af016b7a37d74
ZeWildGuy <njb3112@iCloud.com>
2015-12-30 16:28:20 +1000

    Created PlayerListener class

---
+++ src/uk/co/quartzcraft/coap/listeners/PlayerListener.java
@@ -0,0 +1,11 @@
+package uk.co.quartzcraft.coap.listeners;
+
+import uk.co.quartzcraft.coap.COAP;
+public class PlayerListener {
+
+    private COAP plugin;
+
+    public PlayerListener(COAP pl) {
+        this.plugin = pl;
+    }
+}
