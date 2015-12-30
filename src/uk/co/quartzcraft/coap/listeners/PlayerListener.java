8957e6257e978e80231c85e42c760ef132f5e721
ZeWildGuy <njb3112@iCloud.com>
2015-12-30 16:30:45 +1000

    Player join listener

--- src/uk/co/quartzcraft/coap/listeners/PlayerListener.java
+++ src/uk/co/quartzcraft/coap/listeners/PlayerListener.java
@@ -1,7 +1,18 @@
 package uk.co.quartzcraft.coap.listeners;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import uk.co.quartzcraft.coap.COAP;
+import uk.co.quartzcraft.coap.data.CPlayer;
+import uk.co.quartzcraft.core.util.Util;
+
+import java.util.UUID;
+
 public class PlayerListener implements Listener {
 
     private COAP plugin;
@@ -9,5 +20,12 @@ public class PlayerListener implements Listener {
     public PlayerListener(COAP pl) {
         pl.getServer().getPluginManager().registerEvents(this, pl);
         this.plugin = pl;
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerJoinLow(PlayerJoinEvent join) {
+        Player player = join.getPlayer();
+        CPlayer cPlayer = new CPlayer(player);
+        UUID UUID = player.getUniqueId();
     }
 }
