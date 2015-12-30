768db579bcf31d7cd01c8910df0fb6524f26b04f
ZeWildGuy <njb3112@iCloud.com>
2015-12-30 16:27:39 +1000

    Created CPlayer class

---
+++ src/uk/co/quartzcraft/coap/data/CPlayer.java
@@ -0,0 +1,13 @@
+package uk.co.quartzcraft.coap.data;
+
+import org.bukkit.entity.Player;
+import uk.co.quartzcraft.core.systems.game.GamePlayer;
+import uk.co.quartzcraft.core.systems.game.Rank;
+
+public class CPlayer extends GamePlayer {
+
+    public CPlayer(Player player) {
+
+    }
+
+}
